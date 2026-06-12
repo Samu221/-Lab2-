f18cfc82861b8c3c817bc7accefd2e1331089526
Samuele Toso <samueletoso05@gmail.com>
2026-06-12 10:55:55 +0200

    aggiunti limiti lunghezze, aggiornato readme, sistemati errno

--- src/mr.c
+++ src/mr.c
@@ -413,20 +413,20 @@ int main_function(mr_t mr, const char *input_path,const char *output_path, int m
         token[token_len] = '\0';
 
         //alloco spazio per il risultato
-        char *result = "NULL";
+        char *result = NULL;
+
         if (result_len > 0) {
             result = malloc(result_len);
             if (!result) {
                 free(token);
                 return -1;
             }
-        }
-
-        //leggo il risultato
-        if (readn(reducer_to_main, result, result_len) <= 0) {
-            free(token);
-            free(result);
-            break;
+            
+            if (readn(reducer_to_main, result, result_len) <= 0) {
+                free(token);
+                free(result);
+                break;
+            }
         }
 
         //se ho esaurito lo spazio raddoppio la capacita e alloco nuova memoria
